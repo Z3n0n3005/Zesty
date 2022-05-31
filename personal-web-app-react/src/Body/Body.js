6d91b7e6c7cafec42a814a1f0c7b7c04a70b686e
Luong Tri Vy <trivyluong@gmail.com>
2022-05-31 21:21:31 +0700

    Checkpoint 31/5/2022

--- personal-web-app-react/src/Body/Body.js
+++ personal-web-app-react/src/Body/Body.js
@@ -1,9 +1,14 @@
 import './Body.css';
+import Sidebar from './Sidebar/Sidebar';
+import Content from './Content/Content';
 
 function Body () {
     return (
         <div className='Body'>
-            <p>Quia aut fugiat debitis tenetur enim saepe officiis ad. Quia soluta optio reiciendis eum. Ducimus beatae voluptatem magni qui in sapiente velit. Quae et repellendus est optio. Qui odio id voluptas quos ad eum laborum sint.</p>
+            <Content/>
+            <aside>
+                <Sidebar/>
+            </aside>
         </div>
     )
 }
---
+++ personal-web-app-react/src/Body/Sidebar/Sidebar.js
@@ -0,0 +1,14 @@
+import './Sidebar.css';
+
+const Sidebar = () => {
+    return (
+        <div className = 'Sidebar'>
+            <h2>Contacts</h2>
+            <ul>
+                <li>Github</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar;
