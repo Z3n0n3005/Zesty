8ad3ca9726f2967d35ef3e9391d3f6b0d57e1dcc
Luong Tri Vy <trivyluong@gmail.com>
2022-09-04 11:06:51 +0700

    Zesty project page is up with description

--- personal-web-app-react/src/Main/Content/Projects/Zesty.js
+++ personal-web-app-react/src/Main/Content/Projects/Zesty.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Zesty = () =>{
     return(
         <article>
             <h1>Zesty</h1>
-            <h2>Description</h2>
+            <p>This project is the current website that this content is being hosted on. The reason why it was created is only for getting used to using Javascript, Html,and CSS.</p>
+            <p>Furthermore, I planned to use this website as a platform to host various prototypes of my projects through the use of WebAssembly.</p>
+            <p>For further information about the project, you can check the <a className="normal_link" href="https://github.com/Z3n0n3005/Zesty.git">GitHub</a> repository or the <Link className="normal_link" to="/about">About</Link> page of this website.</p>
         </article>
     )
 }
